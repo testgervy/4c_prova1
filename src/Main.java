2b94c748c1ccbf45206310d9ee35aa891d2396c8
testgervy <test.gervasi@gmail.com>
2022-04-27 12:01:42 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,13 @@
  * 
  * @author antonio
  * 
- * @version 2
+ * @version 2b online
  *
  */
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("prova 2");
+		System.out.println("prova 2b on line");
 
 	}
 
