0acc17e0580cd3831c2accdf040e5a0ff502e745
antonio2 <anton@Surf>
2022-04-27 11:53:59 +0200

    versione 2

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,13 @@
  * 
  * @author antonio
  * 
- * @version 1
+ * @version 2
  *
  */
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("prova 1");
+		System.out.println("prova 2");
 
 	}
 
